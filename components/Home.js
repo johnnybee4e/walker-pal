c3e1084bdfe28a882eeb439fbed13710b1a94e7a
johnnybee4e <jon.rosado42@gmail.com>
2018-06-17 14:36:58 -0400

    basic styling added to home component (check-in/out and sched buttons)

--- components/Home.js
+++ components/Home.js
@@ -1,22 +1,68 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
+  },
+  header: {
+    flex: 0.25,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  headerText: {
+    fontSize: 30,
+    color: 'black',
+    fontFamily: 'Noteworthy-Bold' // Futura, Gurmukhi MN, Heiti SC/TC, Helvetica, Kohinoor Bangla, MarkerFelt-Thin, Menlo, Menlo-Bold, Mishafi, NoteWortyh-Bold/Light, Savoye LET, TrebuchetMS-Bold
+  },
+  homeScreen: {
+    flex: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#dadada',
+  },
+  buttonContainer: {
+    flex: 0.5,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+
+  },
+  button: {
+    fontFamily: 'Noteworthy-Light',
+    width: "40%",
+    height: 40,
+    alignItems: 'center',
+    // justifyContent: 'space-around'
+    justifyContent: 'center',
+    // backgroundColor: 'red',
   },
 });
 
 const Home = ({ navigation }) => (
   <View style={styles.container}>
-    <Text> Welcome to WalkerPal!</Text>
-    <Button
-      onPress={() => navigation.navigate('Scanner')}
-      title="Push To Scan"
-    />
+    <View style={styles.header}>
+      <Text style={styles.headerText}>WalkerPal</Text>
+    </View>
+    <View style={styles.homeScreen}>
+      <Image
+        style={{ resizeMode: 'center' }}
+        source={require('../images/dog-silhouette.jpg')}
+      />
+    </View>
+    <View style={styles.buttonContainer}>
+      <Button
+        style={styles.button}
+        onPress={() => navigation.navigate('Scanner')}
+        title="Check-In/Out"
+      />
+      <Button
+      style={styles.button}
+        onPress={() => console.log('Schedule Selected')}
+        title="Schedule"
+        />
+
+    </View>
   </View>
 );
 
